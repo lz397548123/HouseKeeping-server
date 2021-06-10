c387403bd6d82574ccd97bf6ffaae8685cf78f99
397548123@qq.com <397548123@qq.com>
2021-06-11 04:12:57 +0800

    梁壮-重构

---
+++ src/main/java/com/zzuli/housekeepingserver/service/RolePrivilegeService.java
@@ -0,0 +1,12 @@
+package com.zzuli.housekeepingserver.service;
+/**
+ * 类说明：
+ * Modify Information:
+ * Author        Date           Description
+ * ============ =========== ============================
+ * liang         2021/6/11
+ *
+ */
+    
+    public interface RolePrivilegeService {
+}
---
+++ src/main/java/com/zzuli/housekeepingserver/service/impl/RolePrivilegeServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zzuli.housekeepingserver.service.impl;
+/**
+ * 类说明：
+ * Modify Information:
+ * Author        Date           Description
+ * ============ =========== ============================
+ * liang         2021/6/11
+ *
+ */
+    
+    public class RolePrivilegeServiceImpl {
+}
